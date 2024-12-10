5e234ce489b694600762a1c910e87fdad9e5111b
ce-fello <7552494@gmail.com>
2024-12-10 14:25:57 +0300

    Added files socket_server.py and socket_client.py to track #ABOBACHAT-5

--- src/socket_client.py
+++ src/socket_client.py
@@ -1,28 +1,40 @@
 import socket
-from constants import HOSTNAME, PORT
+from constants import *
 
+class Client:
+    def __init__(self):
+        pass
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  
+    def close_connection(self, client):
+        client.close()
 
-try:
-    client.connect((HOSTNAME, PORT))   
-    print('Connected to', HOSTNAME, PORT)
-except Exception as error:
-    print('Error while connecting to server\n', error)
-    
-while True:
-    message = input()
-    try:
-        data = message.encode()
-        client.send(data)
-        print('Sent data to server')
-    except Exception as error:
-        print('Error while sending data to server\n', error)
-        
-    try:
-        data = client.recv(1024)
-        if not data:
-            break
-        print('Got data from server:', data.decode())
-    except Exception as error:
-        print('Error while recieving data from server', error)
+    def transfer_data(self, client):
+        while True:
+            message = input()
+            try:
+                data = message.encode()
+                client.send(data)
+                if message == 'CLOSECON':
+                    self.close_connection(client)
+                print('Sent data to server')
+            except Exception as error:
+                print('Error while sending data to server\n', error)
+            try:
+                data = client.recv(1024)
+                if not data:
+                    break
+                print('Got data from server:', data.decode())
+            except Exception as error:
+                print('Error while recieving data from server', error)
+
+    def start_connection(self, hostname_to_connect, port):
+        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  
+        try:
+            client.connect((hostname_to_connect, port))   
+            print('Connected to', hostname_to_connect, port)
+        except Exception as error:
+            print('Error while connecting to server\n', error)
+        self.transfer_data(client)
+
+client = Client()
+client.start_connection(HOSTNAME, PORT)
--- src/socket_server.py
+++ src/socket_server.py
@@ -1,64 +1,66 @@
 import socket
-import sqlite3
 from _thread import *
-from constants import HOSTNAME, PORT
+from constants import *
+from db_api import *
+
+class Server:
+	def __init__(self, hostname, port):
+		self.__hostname = hostname
+		self.__port = port
+		self.__connections = {}
+		self.__last_connection_index = 0
+
+	def send_message_to_client():
+		pass
+
+	def run(self, number_of_connections):
+		server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		server.bind((self.__hostname, self.__port))
+		server.listen(number_of_connections)
+		initialize_db()
+		while True:
+			try:
+				connection, address = server.accept()
+				print('Connected succesfully from', address)
+			except Exception as error:
+				print('error while accepting a connection\n', error)
+			try:
+				start_new_thread(client_thread, (connection, ))
+			except Exception as error:
+				print('error while creating new thread', error)
+			try:
+				if str(address) not in self.__connections.values():
+					self.__connections[self.__last_connection_index] = str(address)
+					self.__last_connection_index += 1
+					print('Succesfully added to connections list')
+				else:
+					print('Address already in connections')
+			except Exception as error:
+				print('error while adding to connections list\n', error)
+			print(self.__connections)
 
 
 def client_thread(connection):
-	"""
-	Function for processing the behavior of each client
-	:param connection: tcp connection 
-	:returns: void
-	"""
-	data = connection.recv(1024)
-	message = data.decode()
-	print('Got message from client', message)
-	message_to_client = '200 OK'
-	data_to_client = message_to_client.encode()
-	connection.send(data_to_client)
+	flag_connection_closed = False
+	while not flag_connection_closed:
+		"""
+		Function for processing the behavior of each client
+		:param connection: tcp connection 
+		:returns: void
+		"""
+		data = connection.recv(1024)
+		message = data.decode()
+		print('Got message from client', message)
+		if message == 'CLOSECON':
+			flag_connection_closed = True
+			print('LOG: Connection reset by client')
+		message_to_client = '200 OK'
+		data_to_client = message_to_client.encode()
+		connection.send(data_to_client)
+	connection.close()
 
 
-db_connection = sqlite3.connect('my_database.db')
-cursor = db_connection.cursor()
+last_id = get_last_id()
 
-cursor.execute('''
-CREATE TABLE IF NOT EXISTS Users (
-id INTEGER PRIMARY KEY,
-username TEXT NOT NULL,
-password TEXT NOT NULL
-)
-''')
-
-db_connection.commit()
-db_connection.close()
-
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-connections = {}
-index = 0
-
-server.bind((HOSTNAME, PORT))
-server.listen(5)
-
-while True:
-	try:
-		db_connection, address = server.accept()
-		print('Connected succesfully from', address)
-	except Exception as error:
-		print('error while accepting a connection\n', error)
-
-	try:
-		start_new_thread(client_thread, (db_connection, ))
-	except Exception as error:
-		print('error while creating new thread', error)
-
-	try:
-		if str(address) in connections.values():
-			connections[index] = str(address)
-			index += 1
-			print('Succesfully added to connections list')
-		else:
-			print('Address already in connections')
-	except Exception as error:
-		print('error while adding to connections list\n', error)
-
-	print(connections)
+server = Server(HOSTNAME, PORT)
+server.run(5)
